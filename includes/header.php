3d6b5a2cce16137bd4a52b8b0e839f0019756024
vicilanaya <dynamomelano@yahoo.com>
2020-08-17 19:54:53 -0400

    made exit button responsive

--- includes/header.php
+++ includes/header.php
@@ -1,20 +1,18 @@
-		<header>
-			
-			<button type="button" id="exit" alt="emergency quick exit button">
-				<a href="https://www.google.com/"><strong>Quick EXIT</strong></a>
-			</button>
-			
-			<a href="#content" class="skiplink" id="top">Skip to Content</a>
-			
-			<div>
-				<a href="index.php">
-				
-					<h1>Stay Safe Orlando</h1>
-					
-					<div class="svgbackground"></div>
-			
-				</a>
-			</div>
-			
-		</header>
+<header>
+	
+	<div>
+		<button type="button" id="exit" alt="emergency quick exit button">
+			<a href="https://www.google.com/"><strong>Quick EXIT</strong></a>
+		</button>
+		<a href="#content" class="skiplink" id="top">Skip to Content</a>
+	</div>
+	
+	<div>
+		<a href="index.php">
+			<h1>Stay Safe Orlando</h1>
+			<div class="svgbackground"></div>
+		</a>
+	</div>
+	
+</header>
 		
